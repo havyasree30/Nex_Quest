f44406490e3acfea0da7b345e7f91a307242ef87
Arekatla Nishanth Chowdary <av.sc.u4aie23002@av.students.amrita.edu>
2024-05-07 02:45:23 +0000

    html1

--- Main/script.js
+++ Main/script.js
@@ -1,7 +1,14 @@
-function addBlurExcept(item) {
+window.addEventListener("load", function() {
+    // Add class to main content to trigger animation
+    document.querySelector('main').classList.add('main-visible');
+});
+
+
+function addBlurExcept(event) {
     var gameItems = document.querySelectorAll('.game-item');
+    var targetItem = event.currentTarget;
     gameItems.forEach(item => {
-        if (item !== this) {
+        if (item !== targetItem) {
             item.classList.add('blur');
         }
     });
@@ -17,4 +24,34 @@ function removeBlur() {
 document.querySelectorAll('.game-item').forEach(item => {
     item.addEventListener('mouseover', addBlurExcept);
     item.addEventListener('mouseleave', removeBlur);
+});
+
+document.querySelectorAll('.genre-section').forEach(section => {
+    const scrollContainer = section.querySelector('.game-list');
+    const btnPrev = section.querySelector('.btn-prev');
+    const btnNext = section.querySelector('.btn-next');
+    const scrollStep = 500; // Adjust the scroll step as needed
+
+    btnPrev.addEventListener('click', () => {
+        scrollContainer.scrollBy({
+            left: -scrollStep,
+            behavior: 'smooth'
+        });
+    });
+
+    btnNext.addEventListener('click', () => {
+        scrollContainer.scrollBy({
+            left: scrollStep,
+            behavior: 'smooth'
+        });
+    });
+
+    // Show navigation buttons when game-list is hovered or navigation button is hovered
+    scrollContainer.addEventListener('mouseenter', () => {
+        section.querySelector('.navigation-buttons').style.display = 'flex';
+    });
+
+    section.querySelector('.navigation-buttons').addEventListener('mouseenter', () => {
+        section.querySelector('.navigation-buttons').style.display = 'flex';
+    });
 });
